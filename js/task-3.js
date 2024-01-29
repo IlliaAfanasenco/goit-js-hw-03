d6557f574b2727caae2ce8cda7596eac7a379e4e
IlliaAfanasenco <149933080+IlliaAfanasenco@users.noreply.github.com>
2024-01-29 13:27:07 +0100

    task3

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+  return filteredNumbers;
+}
